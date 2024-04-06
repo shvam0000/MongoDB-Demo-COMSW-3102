0652113a881e13310cebfd783c6d14dd3841b461
shvam0000 <shvam0000@gmail.com>
2024-04-06 14:23:13 -0400

    💄 UI Complete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,9 @@ import Note from './components/note';
 const App = () => {
   const [notes, setNotes] = useState();
 
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+
   const addNote = (e) => {
     e.preventDefault();
     console.log('Note added - ', notes);
@@ -11,16 +14,33 @@ const App = () => {
 
   return (
     <div>
-      <h1>Notes</h1>
-      <form onSubmit={addNote} className="py-10">
-        <label>Enter Note</label>
+      <h1 className="w-screen bg-yellow-400 text-xl font-medium py-4 mx-auto text-center">
+        Notes Keeper
+      </h1>
+      <form
+        onSubmit={addNote}
+        className="py-2 shadow-xl rounded-lg px-5 w-1/3 mx-auto text-left">
         <input
-          value={notes}
-          onChange={(e) => setNotes(e.target.value)}
+          className="block shadow w-full mx-auto px-2 py-2 my-2 rounded-lg"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          required
+          type="text"
+          placeholder="Title"
+        />
+        <textarea
+          placeholder="Write your note here..."
+          rows={4}
+          className="block shadow w-full mx-auto my-2 px-2 py-4 rounded-lg"
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
           required
           type="text"
         />
-        <button type="submit">Create Note</button>
+
+        <button className="bg-yellow-400 px-2 rounded py-1" type="submit">
+          Add Note
+        </button>
       </form>
       <div className="grid grid-cols-4 px-5">
         <div className="p-2">
--- frontend/src/components/note.jsx
+++ frontend/src/components/note.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 
 const Note = (props) => {
+  const handleDelete = () => {
+    props.deleteNote(props.id);
+  };
+
   return (
     <div className="border-2 p-5 w-full rounded">
       <h1 className="flex justify-center items-center py-2 text-xl font-bold">
@@ -8,10 +12,12 @@ const Note = (props) => {
       </h1>
       <h2 className="text-justify">{props.content}</h2>
       <div className="flex justify-center items-center">
-        <button className="py-1 my-2 px-3 bg-blue-400 text-white rounded-lg mx-2">
+        {/* <button className="py-1 my-2 px-3 bg-blue-400 text-white rounded-lg mx-2">
           Mark as done
-        </button>
-        <button className="py-1 my-2 px-3 bg-red-600 text-white rounded-lg mx-2">
+        </button> */}
+        <button
+          onClick={handleDelete}
+          className="py-1 my-2 px-3 bg-red-600 text-white rounded-lg mx-2">
           Delete
         </button>
       </div>
